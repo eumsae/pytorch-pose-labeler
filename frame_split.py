2b8935d1e3b807f92d6f926effc09d15716e4d25
winstr <kimtmdgus22@gmail.com>
2023-01-19 03:59:45 +0000

    ...

--- frame_split.py
+++ frame_split.py
@@ -3,23 +3,37 @@ import os
 from src.files import get_files
 
 
-def del_img_files(target_dir, start_idx:int=None, end_idx:int=None):
+def split_img_files(target_dir, split_idx:int):
     if not os.path.exists(target_dir):
         raise FileNotFoundError(target_dir)
 
     imgs = list(get_files(target_dir, "*.jpg", recursive=False))
     imgs.sort()
 
-    for img in imgs[start_idx:end_idx]:
-        os.remove(img)
-        print(f"{img} was deleted.")
+    imgs_split_1 = imgs[:split_idx]
+    imgs_split_2 = imgs[split_idx:]
+
+    dir_split_1 = target_dir + "_split_1"
+    dir_split_2 = target_dir + "_split_2"
+
+    os.makedirs(dir_split_1)
+    os.makedirs(dir_split_2)
+
+    def rename(imgs, split_dir):
+        for img in imgs:
+            new_img = os.path.basename(img)
+            new_img = os.path.join(split_dir, new_img)
+            os.rename(img, new_img)
+    rename(imgs_split_1, dir_split_1)
+    rename(imgs_split_2, dir_split_2)
+
+    os.system(f"rm -rf {target_dir}")
 
 
 if __name__ == "__main__":
     dataset_dir = "/workspace/data/aihub-human-action-50/action-5-frmext"
     target = "9-6_633"
-    start_idx = 12
-    end_idx = 19
+    split_idx = 6
 
     target_dir_base = target.split("_")[0]
     target_dir_base = os.path.join(
@@ -33,4 +47,4 @@ if __name__ == "__main__":
     for cam in cams:
         target_dir = target_dir_base + cam
         if os.path.exists(target_dir):
-            del_img_files(target_dir, start_idx, end_idx)
+            split_img_files(target_dir, split_idx)
